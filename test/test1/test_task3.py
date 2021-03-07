1bee482860b0a4f1ad48c9b616092ea0dfcdbaf7
AlbertGaz <albert.gazaryan@yandex.ru>
2021-03-07 20:45:36 +0300

    Corrected path of test samples files

--- test/test1/test_task3.py
+++ test/test1/test_task3.py
@@ -10,9 +10,9 @@ from hw.hw1.task3 import find_maximum_and_minimum
 @pytest.mark.parametrize(
     ("value", "expected_result"),
     [
-        ("test_sample_task3_1.txt", (1, 5)),
-        ("test_sample_task3_2.txt", (-1203, 3223)),
-        ("test_sample_task3_3.txt", (5, 5)),
+        ("test/test1/test_samples_task3/test_sample_task3_1.txt", (1, 5)),
+        ("test/test1/test_samples_task3/test_sample_task3_2.txt", (-1203, 3223)),
+        ("test/test1/test_samples_task3/test_sample_task3_3.txt", (5, 5)),
     ],
 )
 def test_min_max(value: str, expected_result: bool):
