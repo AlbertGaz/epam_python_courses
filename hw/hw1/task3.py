5e17eac4c94bb423dd57f91d1aea408c48981a18
AlbertGaz <albert.gazaryan@yandex.ru>
2021-03-08 22:45:33 +0300

    Corrected file reading. data = readlines().

--- hw/hw1/task3.py
+++ hw/hw1/task3.py
@@ -16,15 +16,17 @@ def find_maximum_and_minimum(file_name: str) -> Tuple[int, int]:
 
     """
     with open(file_name) as f:
-        patt = r"-?\d+"
-        maxnum = -float("inf")
-        minnum = float("inf")
+        data = f.readlines()
 
-        for line in f:
-            num_line = re.findall(patt, line)
-            int_line = [int(i) for i in num_line]
-            if maxnum < max(int_line):
-                maxnum = max(int_line)
-            if minnum > min(int_line):
-                minnum = min(int_line)
-        return minnum, maxnum
+    patt = r"-?\d+"
+    maxnum = -float("inf")
+    minnum = float("inf")
+
+    for line in data:
+        num_line = re.findall(patt, line)
+        int_line = [int(i) for i in num_line]
+        if maxnum < max(int_line):
+            maxnum = max(int_line)
+        if minnum > min(int_line):
+            minnum = min(int_line)
+    return minnum, maxnum
