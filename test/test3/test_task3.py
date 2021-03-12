274838a9a78abfd254a844eaa82cbc9b9fc5d14b
AlbertGaz <albert.gazaryan@yandex.ru>
2021-03-12 03:17:41 +0300

    Added task3 and test task3 in hw3

---
+++ test/test3/test_task3.py
@@ -0,0 +1,44 @@
+"""TEST Homework 3.3.
+
+Correct from bugs script.
+"""
+import pytest
+
+from hw.hw3.task3 import make_filter
+
+sample_data = [
+    {
+        "name": "Bill",
+        "last_name": "Gilbert",
+        "occupation": "was here",
+        "type": "person",
+    },
+    {"is_dead": True, "kind": "parrot", "type": "bird", "name": "polly"},
+]
+
+
+@pytest.mark.parametrize(
+    ("arg", "expected_result"),
+    [
+        (
+            {"name": "Bill"},
+            [
+                {
+                    "name": "Bill",
+                    "last_name": "Gilbert",
+                    "occupation": "was here",
+                    "type": "person",
+                }
+            ],
+        ),
+        (
+            {"is_dead": True},
+            [{"is_dead": True, "kind": "parrot", "type": "bird", "name": "polly"}],
+        ),
+        ({"name": "Bill", "is_dead": True}, []),
+    ],
+)
+def test_make_filter(arg, expected_result):
+    actual_result = make_filter(**arg).apply(sample_data)
+
+    assert actual_result == expected_result
