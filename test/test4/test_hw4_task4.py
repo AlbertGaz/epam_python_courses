d14712c52565f73058d8f0ce1c12dcd19882623b
AlbertGaz <albert.gazaryan@yandex.ru>
2021-03-15 02:43:10 +0300

    Modified hw4 task4. Added test hw4 task4

--- test/test4/test_hw4_task4.py
+++ test/test4/test_hw4_task4.py
@@ -8,12 +8,12 @@ from hw.hw4.hw4_task4 import fizzbuzz
     ("n", "expected"),
     [(1, [1]), (10, [1, 2, "fizz", 4, "buzz", "fizz", 7, 8, "fizz", "buzz"])],
 )
-def test_fuzzbuzz_good_input(n, expected):
+def test_fizzbuzz_good_input(n, expected):
     actual_result = fizzbuzz(n)
     assert actual_result == expected
 
 
 @pytest.mark.parametrize(("n"), [(-1), (0), (1.1)])
-def test_fuzzbuzz_bad_input(n):
+def test_fizzbuzz_bad_input(n):
     with pytest.raises(ValueError, match="Input Error!"):
         fizzbuzz(n)
