0c5825e3e630eba0a0f63c80973192df3e18e2d8
AlbertGaz <albert.gazaryan@yandex.ru>
2021-03-15 01:25:40 +0300

    Modified hw4 task4. Added test hw4 task4

--- hw/hw4/hw4_task4.py
+++ hw/hw4/hw4_task4.py
@@ -2,18 +2,49 @@
 
 FizzBuzz numbers.
 """
+import doctest
 from typing import List
 
 
 def fizzbuzz(n: int) -> List[str]:
-    """Get fizzbuzz numbers.
+    r"""Get fizzbuzz numbers.
 
     Args:
         n: number up to get fizzbuzz numbers
 
     Returns: list of fizzbuzz up to n
 
+    - Install Python 3.8 (https://www.python.org/downloads/)
+    - Install pytest `pip install pytest`
+    - Clone the repository <path your repository>
+    - Checkout branch <your branch>
+    - Open terminal
+    - Navigate to where tour python file, is using the commands 'cd' (change directory).
+    For our example
+    > D:\EPAM\courses\epam_python_courses\hw\hw4
+    - Import package doctest 'import doctest'
+    - Run command python -m doctest 'your_file.py'
+
+    >>> fizzbuzz(1)
+    [1]
+
+    >>> fizzbuzz(5)
+    [1, 2, 'fizz', 4, 'buzz']
+
+    >>> fizzbuzz(0)
+    Traceback (most recent call last):
+    ValueError: Input Error!
+
+    >>> fizzbuzz(-100)
+    Traceback (most recent call last):
+    ValueError: Input Error!
+
+    >>> fizzbuzz(1.1)
+    Traceback (most recent call last):
+    ValueError: Input Error!
     """
+    if type(n) != int or n < 1:
+        raise ValueError("Input Error!")
     return [
         "fizzbuzz"
         if i % 15 == 0
@@ -24,3 +55,7 @@ def fizzbuzz(n: int) -> List[str]:
         else i
         for i in range(1, n + 1)
     ]
+
+
+if __name__ == "__main__":
+    doctest.testmod()
