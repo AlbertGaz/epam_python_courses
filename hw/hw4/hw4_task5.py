68d612439213a00b2264fd53fe5e1a1aa74d21e8
AlbertGaz <albert.gazaryan@yandex.ru>
2021-03-15 23:31:27 +0300

    Modified hw4 task5. fizzbuzz = fizz*(condition) + buzz*(condition)

--- hw/hw4/hw4_task5.py
+++ hw/hw4/hw4_task5.py
@@ -14,11 +14,9 @@ def fizzbuzz(n: int) -> Generator:
     Returns: generator
 
     """
-    namenum = (
-        lambda x: (x % 15 == 0 and "fizzbuzz")
-        or (x % 3 == 0 and "fizz")
-        or (x % 5 == 0 and "buzz")
-        or x
-    )
-    for i in map(namenum, range(1, n + 1)):
+
+    def name_num(x: int) -> (int, str):
+        return "fizz" * (x % 3 == 0) + "buzz" * (x % 5 == 0) or x
+
+    for i in map(name_num, range(1, n + 1)):
         yield i
