3233b098bdf69fc8f6e35bc312a0341609eafb08
AlbertGaz <albert.gazaryan@yandex.ru>
2021-03-30 23:45:39 +0300

    Simplified hw7 task2. Used regexp

--- hw/hw7/hw7_task2.py
+++ hw/hw7/hw7_task2.py
@@ -1,4 +1,5 @@
 """Homework 7.2."""
+import re
 
 
 def backspace_compare(first: str, second: str) -> bool:
@@ -11,14 +12,13 @@ def backspace_compare(first: str, second: str) -> bool:
     Returns: bool
 
     """
+    if first == second:
+        return True
+
     res = []
+    patt = r".#"
     for s in [first, second]:
-        if not s:
-            res.append(s)
-            continue
-        s = "".join([s[i] for i in range(len(s) - 1) if s[i + 1] != "#"]) + s[-1] * (
-            s[-1] != "#"
-        )
+        s = re.sub(patt, "", s)
         s = s.replace("#", "")
         res.append(s)
     return res[0] == res[1]
