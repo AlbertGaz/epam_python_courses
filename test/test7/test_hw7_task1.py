6b31c1f24071897b41f9d242344906daaaf91de6
AlbertGaz <albert.gazaryan@yandex.ru>
2021-03-31 15:20:19 +0300

    Created fixture and json file

--- test/test7/test_hw7_task1.py
+++ test/test7/test_hw7_task1.py
@@ -1,39 +1,20 @@
 """TEST Homework 7.1."""
+import json
+
 import pytest
 
 from hw.hw7.hw7_task1 import find_occurrences
 
 
-example_tree_from_task_description = {
-    "first": ["RED", "BLUE"],
-    "second": {
-        "simple_key": ["simple", "list", "of", "RED", "valued"],
-    },
-    "third": {
-        "abc": "BLUE",
-        "jhl": "RED",
-        "complex_key": {
-            "key1": "value1",
-            "key2": ["RED"],
-            "key3": ["a", "lot", "of", "values", {"nested_key": ["RED"]}],
-        },
-    },
-    "fourth": "RED",
-}
-
-example_tree1 = {
-    1: "RED",
-    2: {"RED"},
-    3: ["RED", ["RED", ["RED", ["RED", {4: "RED"}]]]],
-    5: {6: {7: {8: {9: {10: ["RED", "RED", "RED"]}}}}},
-}
+@pytest.fixture()
+def example_tree():
+    with open("example_tree.txt", "r") as f:
+        return json.loads(f.read())
 
 
 @pytest.mark.parametrize(
     ("tree", "element", "expected_result"),
     [
-        (example_tree_from_task_description, "RED", 6),
-        (example_tree1, "RED", 10),
         ([], "RED", 0),
         ({}, "RED", 0),
         (True, True, 1),
@@ -44,3 +25,8 @@ example_tree1 = {
 def test_find_occurrences(tree, element, expected_result):
     actual_result = find_occurrences(tree, element)
     assert actual_result == expected_result
+
+
+def test_find_occurrences_example_tree(example_tree):
+    actual_result = find_occurrences(example_tree, "RED")
+    assert actual_result == 6
