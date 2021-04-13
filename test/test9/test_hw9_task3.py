19c5fc1fd88b7f6c70538ef657a338b7740304da
AlbertGaz <albert.gazaryan@yandex.ru>
2021-04-13 14:54:24 +0300

    Corrected path to dir

--- test/test9/test_hw9_task3.py
+++ test/test9/test_hw9_task3.py
@@ -4,10 +4,12 @@ from hw.hw9.hw9_task3 import universal_file_counter
 
 
 def test_universal_file_counter_none_tokenizer():
-    actual_result = universal_file_counter("test_hw9_task3_samples", "txt")
+    actual_result = universal_file_counter("test/test9/test_hw9_task3_samples", "txt")
     assert actual_result == 12
 
 
 def test_universal_file_counter_tokenizer():
-    actual_result = universal_file_counter("test_hw9_task3_samples", "txt", str.split)
+    actual_result = universal_file_counter(
+        "test/test9/test_hw9_task3_samples", "txt", str.split
+    )
     assert actual_result == 19
