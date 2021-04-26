069bf75babfdea68f6ce1531c0c1fc72d21899d6
AlbertGaz <albert.gazaryan@yandex.ru>
2021-04-26 08:05:46 +0300

    Simplified code

--- hw/hw10/hw10_task1.py
+++ hw/hw10/hw10_task1.py
@@ -72,38 +72,43 @@ async def get_async_responses(url_s: List[str]) -> Tuple:
 def parse_stock_page(html: str) -> Tuple:
     """Parse single stock page to get price, pe, profit, code, name."""
     soup = BeautifulSoup(html, "html.parser")
+
     name = soup.find("span", {"class": "price-section__label"}).text.strip()
+
     code = soup.find("span", {"class": "price-section__category"}).text.strip()
+
     price_raw = soup.find("span", {"class": "price-section__current-value"}).text
     price = float(price_raw.replace(",", "")) * usd_rate
-    price_low_raw = soup.find_all(
-        "div", {"class": "snapshot__data-item snapshot__data-item--small"}
-    )[-1].text.split()[0]
+
+    tag_price_low = "snapshot__data-item snapshot__data-item--small"
+    price_low_raw = soup.find_all("div", {"class": tag_price_low})[-1].text.split()[0]
     price_low = price_low_raw.replace(",", "")
-    price_high_list = soup.find_all(
-        "div",
-        {
-            "class": "snapshot__data-item snapshot__data-item--small snapshot__data-item--right"
-        },
+
+    tag_price_high_list = (
+        "snapshot__data-item snapshot__data-item--small snapshot__data-item--right"
     )
-    price_high_raw = price_high_list[-1].text.split()[0]
+    price_high_raw = soup.find_all("div", {"class": tag_price_high_list})[
+        -1
+    ].text.split()[0]
     price_high = price_high_raw.replace(",", "")
     profit_ratio = 100 * (float(price_high) - float(price_low)) / float(price_low)
+
     pe_raw = soup.find_all("div", {"class": "snapshot__data-item"})[8].text.split()[0]
     pe = pe_raw.replace(",", "")
+
     return name, code[8:], price, profit_ratio, float(pe)
 
 
-def prepare_list_of_dict(
-    data: List, item_name: str, is_reverse: bool = False
-) -> List[Dict]:
+def prepare_out(data: List, item_name: str) -> List[Dict]:
     """Prepare data to be written. Sort and slice."""
+    is_reverse = True
     if item_name == "price":
         item_i = 2
     elif item_name == "potential profit":
         item_i = 3
-    elif item_name == "P/E":
+    elif item_name == "P_E":
         item_i = 4
+        is_reverse = False
     else:
         item_i = 5
     data_sort = sorted(data, key=lambda x: x[item_i], reverse=is_reverse)
@@ -127,26 +132,18 @@ async def main() -> None:
 
         y_growth = get_year_growth_from_main_page(soup)
         y_growths += y_growth
-
-    nhy = [href + [y_gr] for y_gr, href in zip(y_growths, hrefs)]
-
-    stock_urls = [stock_url for _, stock_url, _ in nhy]
+    stock_urls = [stock_url for _, stock_url in hrefs]
     stock_htmls = await get_async_responses(stock_urls)
 
     with ProcessPoolExecutor(max_workers=10) as executor:
         ncppp = executor.map(parse_stock_page, stock_htmls)
-
     data = []
     for y_growth, single_ncppp in zip(y_growths, ncppp):
         data.append(list(single_ncppp) + [float(y_growth[:-1])])
 
-    price_data = prepare_list_of_dict(data, "price", True)
-    pe_data = prepare_list_of_dict(data, "P/E")
-    growth_data = prepare_list_of_dict(data, "growth", True)
-    profit_data = prepare_list_of_dict(data, "potential profit", True)
+    filenames = ["price", "P_E", "growth", "potential profit"]
+    all_data = [prepare_out(data, filename) for filename in filenames]
 
-    filenames = ["price", "pe", "growth", "profit"]
-    all_data = [price_data, pe_data, growth_data, profit_data]
     write_files = [
         asyncio.create_task(async_write_json(data, filename))
         for data, filename in zip(all_data, filenames)
@@ -157,7 +154,6 @@ async def main() -> None:
 main_url = "https://markets.businessinsider.com"
 urls = [main_url + f"/index/components/s&p_500?p={i}" for i in range(1, 11)]
 usd_rate = get_curr_usd_rate()
-
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
